6b9e84657e0b4079dbd9a2c9b236831ae15a23c7
David Mihal <david@dmihal.com>
2021-07-18 12:57:53 +0100

    IPFS Data URIs

--- src/libs/IPFS.ts
+++ src/libs/IPFS.ts
@@ -1,5 +1,7 @@
 import { create, CID } from 'ipfs-http-client';
 import type { IPFS as IPFSClient } from 'ipfs-core-types';
+import uint8ArrayConcat from 'uint8arrays/concat';
+import all from 'it-all';
 
 interface IPFSOptions {
   gateway?: string;
@@ -31,5 +33,11 @@ export class IPFS {
       return content;
     }
     throw new Error(`No files found for CID ${cid.toString()}`);
+  }
+
+  async getDataURI(cid: string | CID, mimeType: string) {
+    const data = uint8ArrayConcat(await all(this.client.cat(cid)));
+    const base64 = Buffer.from(data).toString('base64');
+    return `data:${mimeType};base64,${base64}`;
   }
 }
--- test/libs/IPFS.spec.ts
+++ test/libs/IPFS.spec.ts
@@ -13,4 +13,10 @@ describe('IPFS', function() {
 
     expect(result).to.equal('test123');
   });
+
+  it('should load a data URI', async () => {
+    const uri = await ipfs.getDataURI('QmagaZMuMQKU2a5aJ2VofXBNwHmYpzo8GUR9CCjLWbgxTE', 'image/svg+xml');
+
+    expect(uri).to.equal('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjgiIGhlaWdodD0iMzYiIHZpZXdCb3g9IjAgMCAyOCAzNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTI2LjUxOSAzMy4yMjQyTDEuNDQ3NzUgMjYuMDYxVjkuOTQzODVMMjYuNTE5IDE3LjEwN1YzMy4yMjQyWiIgc3Ryb2tlPSIjMDEwMjAyIiBzdHJva2Utd2lkdGg9IjIuNjg2MiIvPgo8cGF0aCBkPSJNMjYuNTE5IDEuODg1MjVMMS40NDc3NSA5LjA0ODQ1VjI1LjE2NTdMMjYuNTE5IDE4LjAwMjVWMS44ODUyNVoiIHN0cm9rZT0iIzAxMDIwMiIgc3Ryb2tlLXdpZHRoPSIyLjY4NjIiLz4KPC9zdmc+Cg==');
+  });
 });

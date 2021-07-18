93d35b7cd76f718b4381d40eff131a8e8518e166
David Mihal <david@dmihal.com>
2021-07-18 14:05:09 +0100

    Add setup fn directly to module

--- src/Module.ts
+++ src/Module.ts
@@ -2,18 +2,37 @@ import vm from 'vm'
 import { Adapter } from './Adapter'
 import { Context } from './Context'
 
+type SetupFn = (context: Context) => void;
+
+interface ModuleProps {
+  code?: string;
+  setupFn?: SetupFn;
+  context: Context;
+}
+
 export class Module {
-  private code: string;
-  private setupFn: Function | null = null;
+  private code: string | null;
+  private setupFn: SetupFn | null = null;
   public adapters: Adapter[] = [];
   private context: Context;
 
-  constructor(code: string, context: Context) {
-    this.code = code;
+  constructor({ code, setupFn, context }: ModuleProps) {
+    if (code && setupFn) {
+      throw new Error('Can not provide code and setup function');
+    }
+    if (!code && !setupFn) {
+      throw new Error('Must provide at least code or setup fuction');
+    }
+    this.code = code || null;
+    this.setupFn = setupFn || null;
     this.context = context;
   }
 
   evaluate() {
+    if (this.setupFn || !this.code) {
+      throw new Error('Can not evaluate, setup function already set');
+    }
+
     const vmModule: any = { exports: {} };
     const vmContext = vm.createContext({
       exports: vmModule.exports,
@@ -31,7 +50,7 @@ export class Module {
 
   setup() {
     if (!this.setupFn) {
-      throw new Error(`Code not evaluated`)
+      throw new Error('Code not evaluated');
     }
 
     this.setupFn(this.context);
--- test/Module.spec.ts
+++ test/Module.spec.ts
@@ -32,8 +32,47 @@ describe('Module', function() {
       }
     `;
 
-    const polymarketModule = new Module(code, context);
+    const polymarketModule = new Module({ code, context });
     polymarketModule.evaluate();
+    polymarketModule.setup();
+
+    const adapters = list.getAdapters();
+    expect(adapters.length).to.equal(1);
+
+    const metadata = await adapters[0].getMetadata();
+    expect(metadata).to.deep.equal({
+      name: 'Polymarket',
+      icon: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjgiIGhlaWdodD0iMzYiIHZpZXdCb3g9IjAgMCAyOCAzNiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTI2LjUxOSAzMy4yMjQyTDEuNDQ3NzUgMjYuMDYxVjkuOTQzODVMMjYuNTE5IDE3LjEwN1YzMy4yMjQyWiIgc3Ryb2tlPSIjMDEwMjAyIiBzdHJva2Utd2lkdGg9IjIuNjg2MiIvPgo8cGF0aCBkPSJNMjYuNTE5IDEuODg1MjVMMS40NDc3NSA5LjA0ODQ1VjI1LjE2NTdMMjYuNTE5IDE4LjAwMjVWMS44ODUyNVoiIHN0cm9rZT0iIzAxMDIwMiIgc3Ryb2tlLXdpZHRoPSIyLjY4NjIiLz4KPC9zdmc+Cg==',
+    });
+  });
+
+  it('should add adapters to a list using a function', async function () {
+    this.timeout(5000);
+
+    const ipfs = new IPFS();
+    const list = new List('fees');
+    const context = new Context({
+      coinGecko: {} as any,
+      chainData: {} as any,
+      date: {} as any,
+      graph: {} as any,
+      http: {} as any,
+      ipfs,
+      list,
+    });
+
+    const setupFn = function setup(context: Context) {
+      context.register({
+        id: 'polymarket',
+        queries: {},
+        metadata: {
+          name: 'Polymarket',
+          icon: context.ipfs.getDataURILoader('QmagaZMuMQKU2a5aJ2VofXBNwHmYpzo8GUR9CCjLWbgxTE', 'image/svg+xml'),
+        },
+      });
+    };
+
+    const polymarketModule = new Module({ setupFn, context });
     polymarketModule.setup();
 
     const adapters = list.getAdapters();
